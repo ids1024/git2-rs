68f2230005312ac650643d3a07d84f0b6f26ffb9
Ian Douglas Scott <ian@iandouglasscott.com>
2017-06-22 18:06:10 -0700

    Link openssl on Redox

--- libgit2-sys/lib.rs
+++ libgit2-sys/lib.rs
@@ -6,7 +6,7 @@ extern crate libc;
 extern crate libssh2_sys as libssh2;
 #[cfg(feature = "curl")]
 extern crate curl_sys;
-#[cfg(all(unix, not(target_os = "macos"), not(target_os = "ios"), feature = "https"))]
+#[cfg(all(any(unix, target_os = "redox"), not(target_os = "macos"), not(target_os = "ios"), feature = "https"))]
 extern crate openssl_sys;
 extern crate libz_sys as libz;
 
@@ -2642,7 +2642,7 @@ pub fn init() {
     }
 }
 
-#[cfg(all(unix, not(target_os = "macos"), not(target_os = "ios"), feature = "https"))]
+#[cfg(all(any(unix, target_os = "redox"), not(target_os = "macos"), not(target_os = "ios"), feature = "https"))]
 #[doc(hidden)]
 pub fn openssl_init() {
     openssl_sys::init();
